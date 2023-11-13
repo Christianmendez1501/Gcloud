94acd14641b7832541173cec169ac330c6e7478c
ChristianH <Christianmendez1501@gmail.com>
2023-11-13 11:07:27 +0100

    Fith

--- Functions/files/main.py
+++ Functions/files/main.py
@@ -5,6 +5,6 @@ def function_prueba(event, context):
          context (google.cloud.functions.Context): Metadata for the event.
     """
 
-    print("que veremos")
+    print("que veremos again")
     file = event
     print(f"Processing file: {file['name']}.")
