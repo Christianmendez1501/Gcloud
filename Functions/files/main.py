91ef6ae6f36fb718692ba707d9457f9f280cfc29
ChristianH <Christianmendez1501@gmail.com>
2023-11-13 11:13:05 +0100

    Seven

--- Functions/files/main.py
+++ Functions/files/main.py
@@ -5,6 +5,6 @@ def function_prueba(event, context):
          context (google.cloud.functions.Context): Metadata for the event.
     """
 
-    print("que veremos again")
+    print("Vamos que podemos")
     file = event
     print(f"Processing file: {file['name']}.")
