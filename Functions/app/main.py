44e8f632333a66f838bfb5e256da4b325fc30911
ChristianH <Christianmendez1501@gmail.com>
2023-11-13 12:27:26 +0100

    S4

--- Functions/app/main.py
+++ Functions/app/main.py
@@ -5,6 +5,6 @@ def function_prueba(event, context):
          context (google.cloud.functions.Context): Metadata for the event.
     """
 
-    print("Vamos campeonn1")
+    print("Vamos campeonnn1")
     file = event
     print(f"Processing file: {file['name']}.")
